9b3d3826405525d4e0874ea886a1f552c58a4e0b
Kevin Smith <ksmithbaylor@gmail.com>
2016-04-07 11:33:24 -0400

    Set up initial structure with testing

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function karmaConfig(config) {
+  config.set({
+    plugins: [
+      require('karma-tap'),
+      require('karma-chrome-launcher'),
+      require('karma-webpack')
+    ],
+
+    basePath: '',
+    frameworks: ['tap'],
+    files: [
+      'tests.js'
+    ],
+
+    preprocessors: {
+      'tests.js': ['webpack']
+    },
+
+    webpack: {
+      node: {
+        fs: 'empty'
+      }
+    },
+
+    reporters: ['dots'],
+    port: 8888,
+    colors: true,
+    logLevel: config.LOG_ERROR,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: true
+  });
+};
---
+++ tests.js
@@ -0,0 +1,6 @@
+const test = require('tape');
+
+test('it works', t => {
+  t.pass();
+  t.end();
+});
