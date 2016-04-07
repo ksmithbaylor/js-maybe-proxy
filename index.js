d2c646a9b8ede133b52c9e1100210ddbc52f0e9a
Kevin Smith <ksmithbaylor@gmail.com>
2016-04-07 16:03:47 -0400

    Add and_then function

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+const Target = Object.create(null);
+Object.assign(Target, {
+  and_then(f) {
+    if (this.value !== null && this.value !== undefined) {
+      return f(this.value);
+    }
+
+    return Maybe(this.value);
+  }
+});
+
 function Maybe(value) {
-  const target = {
-    value: value
-  };
+  const target = Object.create(Target);
+  Object.assign(target, {
+    value,
+    get fromMaybe() { return value; }
+  });
+
   return new Proxy(target, {
     get(target, name) {
-      if (name === 'fromMaybe') {
-        return target.value;
+      if (name in target) {
+        return Reflect.get(target, name);
       }
 
       return null;
--- tests.js
+++ tests.js
@@ -10,3 +10,8 @@ test('Maybe lets me get the value', t => {
   t.assert(42 === Maybe(42).fromMaybe);
   t.end();
 });
+
+test('Maybe lets me call and_then', t => {
+  t.assert(43 === Maybe(42).and_then(x => Maybe(x + 1)).fromMaybe);
+  t.end();
+});
